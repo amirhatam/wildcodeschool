f34289016ea5fc1c48c43766db64336f3dc653d3
Amir <amirhatam136@gmail.com>
2022-04-30 14:28:10 +0200

    create server and connect to database

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const router = express.Router()
+
+mongoose.connect('mongodb://localhost:27017/argonaute', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("I'm connected to the database")
+    }
+})
+
+const port = 8080
+
+const app = express()
+app.use(express.json())
+
+app.use("/", router)
+
+
+app.listen(port, () => {
+    console.log("Server is listening at port ", port);
+})
